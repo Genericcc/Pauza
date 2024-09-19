58076106a9ce43bd5e97b1252956ac7ced2ee336
Bolan <m.bolan.m@gmail.com>
2024-09-19 21:08:07 +0200

    SpawnItems and foliage

---
+++ Assets/Scripts/LevelPOIs/ItemSpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Items;
+
+using Managers;
+
+using UnityEngine;
+
+namespace LevelPOIs
+{
+    public class ItemSpawnPoint : MonoBehaviour
+    {
+        [SerializeField]
+        public Item itemPrefab;
+
+        private Item _itemGameObject;
+
+        public Item SpawnItem()
+        {
+            _itemGameObject = Instantiate(itemPrefab);
+            
+            return _itemGameObject;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Items;
+
+using LevelPOIs;
+
+using UnityEngine;
+
+namespace Managers
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public static LevelManager Instance;
+        
+        [SerializeField]
+        public List<Item> itemsToSpawn;
+        
+        [SerializeField]
+        public List<ItemSpawnPoint> itemSpawnPoints;
+
+        private List<Item> _spawnedItems;
+
+        public LevelManager(List<Item> spawnedItems)
+        {
+            _spawnedItems = spawnedItems;
+        }
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            SpawnItems();
+        }
+
+        public void SpawnItems()
+        {
+            foreach (var itemSpawnPoint in itemSpawnPoints)
+            {
+                var newItem = itemSpawnPoint.SpawnItem();
+                _spawnedItems.Add(newItem);
+            }
+        }
+    }
+}
