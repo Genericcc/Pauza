956a816891b201375991c7555667f88ffd535f70
Bolan <m.bolan.m@gmail.com>
2024-09-20 01:02:46 +0200

    Clock

---
+++ Assets/Scripts/Clock/ClockManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace Clock
+{
+    public class ClockManager : MonoBehaviour
+    {
+        [SerializeField]
+        public GameObject minutePivot;
+        
+        [SerializeField]
+        public GameObject hourPivot;
+
+        [SerializeField]
+        private float minuteSpeed;
+        
+        [SerializeField]
+        private float hourSpeed;
+
+        private void Update()
+        {
+            minutePivot.transform.Rotate(Vector3.up, Time.deltaTime * minuteSpeed);
+            hourPivot.transform.Rotate(Vector3.up, Time.deltaTime * hourSpeed);
+        }
+    }
+}
